935cf16d32f36345d2e84609b862a741781d593e
Aaron Riggs <arsgate@gmail.com>
2017-09-10 18:18:50 -0500

    Changed Group Name

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 //  Online Shopping System
 //
 //  Created by Aaron Turner, Aaron Riggs and Scott Reilman on 9/10/17.
-//  Copyright © 2017 Aaron Turner. All rights reserved.
+//  Copyright © 2017 AARONS. All rights reserved.
 //
 
 #include <iostream>
