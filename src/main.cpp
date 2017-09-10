a9b1055f13139009c80456a727457d162c05b524
Airboy98 <aaron.turner@ttu.edu>
2017-09-10 18:56:02 -0500

    updating function name

--- src/main.cpp
+++ src/main.cpp
@@ -9,36 +9,59 @@
 #include <iostream>
 using namespace std;
 
+//MAIN SEQUENCE
+
+//FUNCTION 1
+//Customer provides order request and customer account Id to pay for purchase
 void orderReq()
 {
     
 }
 
+//FUNCTION 2
+// System retrieves customer account information, including the customer’s credit card details.
 void pullData()
 {
     
 }
 
+//FUNCTION 3
+//System requests to a bank checking the customer’s credit card for the purchase amount and, if approved, creates a credit card purchase authorization number.
 void bankCheck()
 {
     
 }
 
+//FUNCTION 4
+//System creates a delivery order containing order details, customer Id, and credit card authorization number
 void createOrder()
 {
     
 }
 
+//FUNCTION 5
+//System confirms approval of purchase and displays order information to customer.
 void displayConfirmation()
 {
 
 }
 
+//ALTERNATIVE SEQUENCES
+
+//FUNCTION 1
+//If customer does not have an account, the system prompts the
+//customer to provide information in order to create a new account. The
+//customer can either enter the account information or cancel the order.
 void noAccount()
 {
 
 }
 
+//FUNCTION 2
+// If authorization of the customer’s credit card is denied (e.g., invalid
+//credit card or insufficient funds in the customer’s credit card account), the
+//system prompts the customer to enter a different credit card number. The
+//customer can either enter a different credit card number or cancel the order.
 void denied()
 {
 
