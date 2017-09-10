222a2fc015cb8a51b3d65cad5b33182941360a9c
Airboy98 <31832185+Airboy98@users.noreply.github.com>
2017-09-10 18:09:19 -0500

    Add files via upload
    first iteration of main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+//
+//  main.cpp
+//  Online Shopping System
+//
+//  Created by Aaron Turner on 9/10/17.
+//  Copyright © 2017 Aaron Turner. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+void orderReq()
+{
+    
+}
+
+void pullData()
+{
+    
+}
+
+void bankCheck()
+{
+    
+}
+
+void createOrder()
+{
+    
+}
+
+
+
+
+int main()
+{
+    
+}
