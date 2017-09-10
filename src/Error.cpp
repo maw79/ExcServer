ccec6347b1ec362658657dffa31c1a8562968f99
Aaron Riggs <arsgate@gmail.com>
2017-09-10 18:46:46 -0500

    Added Error

---
+++ src/Error.cpp
@@ -0,0 +1,5 @@
+//
+// Created by arsga on 9/10/2017.
+//
+
+#include "Error.h"
---
+++ src/Error.h
@@ -0,0 +1,8 @@
+//
+// Created by arsga on 9/10/2017.
+//
+
+#ifndef EXCSERVER_ERROR_H
+#define EXCSERVER_ERROR_H
+
+#endif //EXCSERVER_ERROR_H
