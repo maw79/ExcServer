92108fba4e02358153a8b43631133f39a699695b
Airboy98 <aaron.turner@ttu.edu>
2017-09-10 20:44:42 -0500

    order request function shit v3

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@
 
 #include <iostream>
 #include <string>
-#import "Menu.h"
-#import "Error.h"
+#include "Menu.h"
+#include "Error.h"
 using namespace std;
 
 //MAIN SEQUENCE
@@ -34,7 +34,7 @@ void orderReq()
     if(productID != null)
     {
         cout << "Specify Amount:";
-        cin >> amount;
+        cin >> productAmount;
     }
     else
     {
