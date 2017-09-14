a00ecb3002eb96bb1265ae051e8b0ed0911c117c
Aaron Riggs <arsgate@gmail.com>
2017-09-14 10:17:24 -0500

    Even more customer changes

--- src/Customer.cpp
+++ src/Customer.cpp
@@ -14,12 +14,12 @@ int CC1Array[] = {1234,3423,5555,7830,1357};
 int CC2Array[] = {1334,3456,5123,7840,2377};
 int CC3Array[] = {1567,3246,5658,7850,3347};
 int CC4Array[] = {3244,3996,5348,7860,4387};
-long long int accountArray[]={};
+long long int accountArray[]={1234567890,6789012345,9087654321,7850276589,4543673492};
 
 Customer::Customer(int nums)
 {
     Customer::name = nameArray[nums];
-    Customer::accountID = rand() % 8999999999 + 1000000000;
+    Customer::accountID = accountArray[nums];
     Customer::address = addressArray[nums];
     Credit CC;
     CC.setZip(zipArray[nums]);
@@ -27,7 +27,6 @@ Customer::Customer(int nums)
     CC.setCCnum(CC1Array[nums], CC2Array[nums], CC3Array[nums], CC4Array[nums]);
     Customer::creditC = CC;
     Customer::accBalance = accountBallanceArray[nums];
-    nums++;
 }
 
 Customer::Customer(string name, string address, long long int accountID, Credit creditC, int accBalance)
